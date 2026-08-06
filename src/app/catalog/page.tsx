42dd3d9acfdba589a86cefa960749d8c45fc36d0
MopuK <stanislavgusev.sv@gmail.com>
2026-08-06 23:06:55 +0300

    fix: add suspense for catalog page

--- src/app/catalog/page.tsx
+++ src/app/catalog/page.tsx
@@ -1,10 +1,13 @@
 import CatalogList from "@/src/features/catalog/ui/CatalogList";
 import FiltersSidebar from "@/src/features/product-filters/ui/FiltersSidebar";
+import { Suspense } from "react";
 
 export default async function page() {
   return (
     <div className="min-h-screen px-10 grid grid-cols-1 xl:grid-cols-[1fr_3fr]">
-      <FiltersSidebar />
+      <Suspense fallback={<div>Loading...</div>}>
+        <FiltersSidebar />
+      </Suspense>
       <CatalogList />
     </div>
   );
