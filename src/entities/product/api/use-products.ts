abfb285ddd60862b1592b93be2399a870fdec5ef
MopuK <stanislavgusev.sv@gmail.com>
2026-08-18 11:32:49 +0300

    fix: backend's url that is fetched to not include any additional chars

--- src/entities/product/api/use-products.ts
+++ src/entities/product/api/use-products.ts
@@ -11,7 +11,7 @@ export default function useProductVariantsQuery() {
     queryKey: ["product_variants", filters],
     queryFn: async (): Promise<ProductVariantListResponse> => {
       const response = await fetch(`
-        ${process.env.NEXT_PUBLIC_BACKEND_API}/api/v1/products?${searchParams.toString()}}
+        ${process.env.NEXT_PUBLIC_BACKEND_API}/api/v1/products?${searchParams.toString()}
       `);
       return await response.json();
     },
