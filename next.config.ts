f2ace78101b4c4c113f36f46c2616e76e290da2f
MopuK <stanislavgusev.sv@gmail.com>
2026-08-18 11:28:02 +0300

    fix: add localhost to allowedDevOrigins

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  allowedDevOrigins: ["127.0.0.1"],
   images: {
     remotePatterns: [
       new URL("https://shop-backend-djyw.onrender.com/static/images/**"),
